9468ebec6d0c6d1f2f1387c14d248da0cfd8a364
Oluwatosin <kingosenitosin@gmail.com>
2024-08-06 01:51:02 -0500

    used product db and removed personal

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price_cents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    price_cents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    price_cents: 799
-}
-];
 
 let productsHTML = '';
 
@@ -49,7 +23,7 @@ products.forEach((product) => {
                     <div class="product-rating-count link-primary">${product.rating.count}</div>
                 </div>
 
-                <div class="product-price">$${(product.price_cents / 100).toFixed(2)}</div>
+                <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
 
                 <div class="product-quantity-container">
                     <select>
