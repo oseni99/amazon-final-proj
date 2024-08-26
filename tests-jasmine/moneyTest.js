694c71f5bdaf4d5a484c65708549f034a6a6523d
Oluwatosin <kingosenitosin@gmail.com>
2024-08-26 15:04:31 -0500

    Removed jasmine default test and add custom tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+
+describe("test suite: formatCurrency", () => {
+    it("Converts Cents into dollars", () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it("Works with zeros", () => {
+        expect(formatCurrency(0)).toEqual("0.00");
+    });
+    it("Rounds up to the nearest cent", () => {
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+})
